75a3f448cbb870cb666efd76960db41d774213a3
Qt <kylinmania@163.com>
2022-01-20 14:40:55 +0800

    add polaris bom

---
+++ base/polaris-bom/polaris-build-tools/src/test/java/com/librecoder/awesome/checkstyle/Test1Check.java
@@ -0,0 +1,24 @@
+package com.librecoder.awesome.checkstyle;
+
+import com.puppycrawl.tools.checkstyle.api.AbstractCheck;
+
+/**
+ * @author Qt
+ * @version Jun 17, 2021
+ */
+public class Test1Check extends AbstractCheck {
+	@Override
+	public int[] getDefaultTokens() {
+		return new int[0];
+	}
+
+	@Override
+	public int[] getAcceptableTokens() {
+		return new int[0];
+	}
+
+	@Override
+	public int[] getRequiredTokens() {
+		return new int[0];
+	}
+}
