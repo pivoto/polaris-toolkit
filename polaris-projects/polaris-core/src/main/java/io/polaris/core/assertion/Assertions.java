0cfb1d2198ef333a356101e8786906bf4acb2968
tianqiang <kylinmania@163.com>
2024-01-07 21:22:30 +0800

    3.1.9-SNAPSHOT Assertions

--- polaris-projects/polaris-core/src/main/java/io/polaris/core/assertion/Assertions.java
+++ polaris-projects/polaris-core/src/main/java/io/polaris/core/assertion/Assertions.java
@@ -326,7 +326,7 @@ public class Assertions {
 		}
 	}
 
-	public static <T> void assertInstanceOf(Class<T> expectedType, Object actualValue, String message) throws AssertionException {
+	public static void assertInstanceOf(Class<?> expectedType, Object actualValue, String message) throws AssertionException {
 		if (!expectedType.isInstance(actualValue)) {
 			String reason = (actualValue == null ? "Unexpected null value" : "Unexpected type");
 			throw new AssertionException(buildPrefix(message)
@@ -335,7 +335,7 @@ public class Assertions {
 		}
 	}
 
-	public static <T> void assertNotInstanceOf(Class<T> expectedType, Object actualValue, String message) throws AssertionException {
+	public static void assertNotInstanceOf(Class<?> expectedType, Object actualValue, String message) throws AssertionException {
 		if (expectedType.isInstance(actualValue)) {
 			throw new AssertionException(buildPrefix(message)
 				+ format(Other.of(expectedType), actualValue.getClass(), "Unexpected type"),
@@ -343,4 +343,178 @@ public class Assertions {
 		}
 	}
 
+	public static void assertAssignable(Class<?> expectedType, Class<?> actualValue, String message) throws AssertionException {
+		if (actualValue == null || !expectedType.isAssignableFrom(actualValue)) {
+			String reason = (actualValue == null ? "Unexpected null value" : "Unexpected type");
+			throw new AssertionException(buildPrefix(message)
+				+ format(expectedType, actualValue == null ? null : actualValue, reason),
+				expectedType, actualValue == null ? null : actualValue);
+		}
+	}
+
+	public static void assertNotAssignable(Class<?> expectedType, Class<?> actualValue, String message) throws AssertionException {
+		if (actualValue != null && expectedType.isAssignableFrom(actualValue)) {
+			throw new AssertionException(buildPrefix(message)
+				+ format(Other.of(expectedType), actualValue, "Unexpected type"),
+				Other.of(expectedType), actualValue);
+		}
+	}
+
+
+	public static void isTrue(boolean condition, String message) throws IllegalArgumentException {
+		if (!condition) {
+			throw new IllegalArgumentException(message);
+		}
+	}
+
+	public static void isFalse(boolean condition, String message) throws IllegalArgumentException {
+		if (condition) {
+			throw new IllegalArgumentException(message);
+		}
+	}
+
+	public static void isNull(Object actual, String message) throws IllegalArgumentException {
+		if (actual != null) {
+			throw new IllegalArgumentException(message);
+		}
+	}
+
+	public static void notNull(Object actual, String message) throws IllegalArgumentException {
+		if (actual == null) {
+			throw new IllegalArgumentException(message);
+		}
+	}
+
+	public static void isEquals(char expected, char actual, String message) throws IllegalArgumentException {
+		if (expected != actual) {
+			throw new IllegalArgumentException(message);
+		}
+	}
+
+	public static void isEquals(byte expected, byte actual, String message) throws IllegalArgumentException {
+		if (expected != actual) {
+			throw new IllegalArgumentException(message);
+		}
+	}
+
+	public static void isEquals(short expected, short actual, String message) throws IllegalArgumentException {
+		if (expected != actual) {
+			throw new IllegalArgumentException(message);
+		}
+	}
+
+	public static void isEquals(int expected, int actual, String message) throws IllegalArgumentException {
+		if (expected != actual) {
+			throw new IllegalArgumentException(message);
+		}
+	}
+
+	public static void isEquals(long expected, long actual, String message) throws IllegalArgumentException {
+		if (expected != actual) {
+			throw new IllegalArgumentException(message);
+		}
+	}
+
+	public static void isEquals(double expected, double actual, String message) throws IllegalArgumentException {
+		if (expected != actual) {
+			throw new IllegalArgumentException(message);
+		}
+	}
+
+	public static void isEquals(float expected, float actual, String message) throws IllegalArgumentException {
+		if (expected != actual) {
+			throw new IllegalArgumentException(message);
+		}
+	}
+
+	public static void isEquals(Object expected, Object actual, String message) throws IllegalArgumentException {
+		if (!Objects.equals(expected, actual)) {
+			throw new IllegalArgumentException(message);
+		}
+	}
+
+	public static void isSame(Object expected, Object actual, String message) throws IllegalArgumentException {
+		if (expected != actual) {
+			throw new IllegalArgumentException(message);
+		}
+	}
+
+	public static void notEquals(char expected, char actual, String message) throws IllegalArgumentException {
+		if (expected == actual) {
+			throw new IllegalArgumentException(message);
+		}
+	}
+
+	public static void notEquals(byte expected, byte actual, String message) throws IllegalArgumentException {
+		if (expected == actual) {
+			throw new IllegalArgumentException(message);
+		}
+	}
+
+	public static void notEquals(short expected, short actual, String message) throws IllegalArgumentException {
+		if (expected == actual) {
+			throw new IllegalArgumentException(message);
+		}
+	}
+
+	public static void notEquals(int expected, int actual, String message) throws IllegalArgumentException {
+		if (expected == actual) {
+			throw new IllegalArgumentException(message);
+		}
+	}
+
+	public static void notEquals(long expected, long actual, String message) throws IllegalArgumentException {
+		if (expected == actual) {
+			throw new IllegalArgumentException(message);
+		}
+	}
+
+	public static void notEquals(double expected, double actual, String message) throws IllegalArgumentException {
+		if (expected == actual) {
+			throw new IllegalArgumentException(message);
+		}
+	}
+
+	public static void notEquals(float expected, float actual, String message) throws IllegalArgumentException {
+		if (expected == actual) {
+			throw new IllegalArgumentException(message);
+		}
+	}
+
+
+	public static void notEquals(Object expected, Object actual, String message) throws IllegalArgumentException {
+		if (Objects.equals(expected, actual)) {
+			throw new IllegalArgumentException(message);
+		}
+	}
+
+	public static void notSame(Object expected, Object actual, String message) throws IllegalArgumentException {
+		if (expected == actual) {
+			throw new IllegalArgumentException(message);
+		}
+	}
+
+	public static void isInstanceOf(Class<?> expectedType, Object actualValue, String message) throws IllegalArgumentException {
+		if (!expectedType.isInstance(actualValue)) {
+			throw new IllegalArgumentException(message);
+		}
+	}
+
+	public static void notInstanceOf(Class<?> expectedType, Object actualValue, String message) throws IllegalArgumentException {
+		if (expectedType.isInstance(actualValue)) {
+			throw new IllegalArgumentException(message);
+		}
+	}
+
+	public static void isAssignable(Class<?> expectedType, Class<?> actualValue, String message) throws IllegalArgumentException {
+		if (actualValue == null || !expectedType.isAssignableFrom(actualValue)) {
+			throw new IllegalArgumentException(message);
+		}
+	}
+
+	public static void notAssignable(Class<?> expectedType, Class<?> actualValue, String message) throws IllegalArgumentException {
+		if (actualValue != null && expectedType.isAssignableFrom(actualValue)) {
+			throw new IllegalArgumentException(message);
+		}
+	}
 }
