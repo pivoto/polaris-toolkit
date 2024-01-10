d87cdb71a6beabbd996f60ab7a39c377870072c5
tianqiang <kylinmania@163.com>
2024-01-10 14:11:03 +0800

    3.1.11

--- .bin/mvn-version.cmd
+++ .bin/mvn-version.cmd
@@ -1,9 +1,9 @@
 @echo off
-REM set VER=3.1.11-SNAPSHOT
-set VER=3.1.10
 set GIT_BASH=D:\devel\shell\Git\bin\bash
+REM set VER=3.1.11-SNAPSHOT
+REM call mvn -f ../pom.xml versions:set -DnewVersion=%VER%
+REM call mvn -f ../pom.xml versions:commit
+REM %GIT_BASH% -c 'sed -i -r -e  "s/<project.polaris.revision>.+<\/project.polaris.revision>/<project.polaris.revision>%VER%<\/project.polaris.revision>/g" ../pom.xml'
 
-call mvn -f ../pom.xml versions:set -DnewVersion=%VER%
-call mvn -f ../pom.xml versions:commit
-%GIT_BASH% -c 'sed -i -r -e  "s/<project.polaris.revision>.+<\/project.polaris.revision>/<project.polaris.revision>%VER%<\/project.polaris.revision>/g" ../pom.xml'
+%GIT_BASH% -c 'sh mvn-version.sh'
 pause
