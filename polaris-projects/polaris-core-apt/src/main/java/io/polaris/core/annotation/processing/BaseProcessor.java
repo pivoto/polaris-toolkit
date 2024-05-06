32d668c9afbe28e0bc43ab04676379981218d909
tianqiang <kylinmania@163.com>
2024-05-06 16:19:50 +0800

    3.1.34-SNAPSHOT update BaseProcessor

--- polaris-projects/polaris-core-apt/src/main/java/io/polaris/core/annotation/processing/BaseProcessor.java
+++ polaris-projects/polaris-core-apt/src/main/java/io/polaris/core/annotation/processing/BaseProcessor.java
@@ -1,10 +1,8 @@
 package io.polaris.core.annotation.processing;
 
-import com.sun.tools.javac.api.JavacTrees;
-import com.sun.tools.javac.processing.JavacProcessingEnvironment;
-import com.sun.tools.javac.tree.TreeMaker;
-import com.sun.tools.javac.util.Context;
-import com.sun.tools.javac.util.Names;
+
+import java.io.InputStream;
+import java.util.Properties;
 
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.Filer;
@@ -14,8 +12,6 @@ import javax.lang.model.util.Elements;
 import javax.lang.model.util.Types;
 import javax.tools.FileObject;
 import javax.tools.StandardLocation;
-import java.io.InputStream;
-import java.util.Properties;
 
 /**
  * @author Qt
@@ -27,12 +23,12 @@ public abstract class BaseProcessor extends AbstractProcessor {
 	protected ProcessingEnvironment env;
 	/** 用于在编译器打印消息的组件 */
 	protected Messager messager;
-	/** 语法树 */
-	protected JavacTrees trees;
-	/** 用来构造语法树节点 */
-	protected TreeMaker treeMaker;
-	/** 用于创建标识符的对象 */
-	protected Names names;
+//	/** 语法树 */
+//	protected JavacTrees trees;
+//	/** 用来构造语法树节点 */
+//	protected TreeMaker treeMaker;
+//	/** 用于创建标识符的对象 */
+//	protected Names names;
 	/** 用于将创建的类写入到文件 */
 	protected Filer filer;
 	protected Elements elements;
@@ -44,10 +40,10 @@ public abstract class BaseProcessor extends AbstractProcessor {
 	public synchronized void init(ProcessingEnvironment processingEnv) {
 		this.env = processingEnv;
 		this.messager = processingEnv.getMessager();
-		this.trees = JavacTrees.instance(processingEnv);
-		Context context = ((JavacProcessingEnvironment) processingEnv).getContext();
-		this.treeMaker = TreeMaker.instance(context);
-		this.names = Names.instance(context);
+//		this.trees = JavacTrees.instance(processingEnv);
+//		Context context = ((JavacProcessingEnvironment) processingEnv).getContext();
+//		this.treeMaker = TreeMaker.instance(context);
+//		this.names = Names.instance(context);
 		this.filer = processingEnv.getFiler();
 		this.elements = processingEnv.getElementUtils();
 		this.types = processingEnv.getTypeUtils();
