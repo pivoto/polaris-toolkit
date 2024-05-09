157aa312c46303568d36d19bea3f6da47ee47ba4
tianqiang <kylinmania@163.com>
2024-05-09 10:10:02 +0800

    3.1.37-SNAPSHOT

--- .bin/mvn-version.sh
+++ .bin/mvn-version.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-BETA=0
-VER=3.1.36
+BETA=1
+VER=3.1.37
 if [[ ${BETA} -eq 1 ]];then
 	VER=${VER}-SNAPSHOT
 fi
 
-mvn -f ../pom.xml versions:set -DnewVersion=${VER}
-mvn -f ../pom.xml versions:commit
+mvn -f ../pom.xml -P withDemo,ide-idea versions:set -DnewVersion=${VER}
+mvn -f ../pom.xml -P withDemo,ide-idea versions:commit
 sed -i -r -e  "s/<project.polaris.revision>.+<\/project.polaris.revision>/<project.polaris.revision>${VER}<\/project.polaris.revision>/g" ../pom.xml
 #echo "wait 5s...."
 #sleep 5
