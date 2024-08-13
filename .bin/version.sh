59a8ec64f5b677c5cba3cbcd355d68e946ddb291
tianqiang <kylinmania@163.com>
2024-08-13 17:57:56 +0800

    release 3.2.15

--- .bin/version.sh
+++ .bin/version.sh
@@ -4,16 +4,14 @@ WORK_DIR=${PWD}
 SHELL_DIR=$(cd "$(dirname "$0")" && pwd -P)
 cd "${SHELL_DIR}"/ || exit
 
-BETA=1
+BETA=0
 VER=3.2.15
 if [[ ${BETA} -eq 1 ]];then
 	VER=${VER}-SNAPSHOT
 fi
 
-WORK_DIR=${PWD}
-SHELL_DIR=$(cd $(dirname $0) && pwd -P)
-cd ${SHELL_DIR}
 
+echo $PWD
 mvn -f ../pom.xml -P withDemo,withIdea versions:set -DnewVersion=${VER}
 mvn -f ../pom.xml -P withDemo,withIdea versions:commit
 sed -i -r -e  "s/<project.polaris-toolkit.revision>.+<\/project.polaris-toolkit.revision>/<project.polaris-toolkit.revision>${VER}<\/project.polaris-toolkit.revision>/g" ../pom.xml
