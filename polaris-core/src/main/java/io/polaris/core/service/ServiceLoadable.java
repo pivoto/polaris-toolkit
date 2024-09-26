b6b31b2a11fc05e0ae7ebb2b66089bf2371ddd4c
tianqiang <kylinmania@163.com>
2024-09-26 11:32:24 +0800

    fea 添加SPI标识接口

---
+++ polaris-core/src/main/java/io/polaris/core/service/ServiceLoadable.java
@@ -0,0 +1,12 @@
+package io.polaris.core.service;
+
+/**
+ * 标识为一个SPI服务类。可通过{@link ServiceLoader}加载
+ * <p>
+ * 仅作标识，无其他限制
+ *
+ * @author Qt
+ * @since Sep 26, 2024
+ */
+public interface ServiceLoadable {
+}
---
+++ polaris-core/src/main/java/io/polaris/core/service/StatefulServiceLoadable.java
@@ -0,0 +1,12 @@
+package io.polaris.core.service;
+
+/**
+ * 标识为一个有状态的SPI服务类。可通过{@link StatefulServiceLoader}加载
+ * <p>
+ * 仅作标识，无其他限制
+ *
+ * @author Qt
+ * @since Sep 26, 2024
+ */
+public interface StatefulServiceLoadable {
+}
