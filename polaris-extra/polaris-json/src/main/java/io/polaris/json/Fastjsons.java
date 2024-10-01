ed59815a6990e1db296310c36d6fbed3a550f8a6
tianqiang <kylinmania@163.com>
2024-10-01 10:23:41 +0800

    【代码优化】 优化工具类Fastjsons

--- polaris-extra/polaris-json/src/main/java/io/polaris/json/Fastjsons.java
+++ polaris-extra/polaris-json/src/main/java/io/polaris/json/Fastjsons.java
@@ -1,13 +1,21 @@
 package io.polaris.json;
 
-import com.alibaba.fastjson2.*;
+import java.lang.reflect.Type;
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.List;
+
 import io.polaris.core.lang.JavaType;
 import io.polaris.core.lang.TypeRef;
 import io.polaris.core.service.StatefulServiceLoader;
 import lombok.extern.slf4j.Slf4j;
 
-import java.lang.reflect.Type;
-import java.util.List;
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONArray;
+import com.alibaba.fastjson2.JSONObject;
+import com.alibaba.fastjson2.JSONReader;
+import com.alibaba.fastjson2.JSONWriter;
+import com.alibaba.fastjson2.TypeReference;
 
 /**
  * @author Qt
@@ -45,9 +53,29 @@ public class Fastjsons {
 			try {
 				customizer.customize();
 			} catch (Throwable e) {
-				log.error("" , e);
+				log.error("", e);
 			}
 		}
+	}
+
+	static JSONWriter.Feature[] merge(JSONWriter.Feature[] standard, final JSONWriter.Feature[] features) {
+		if (features.length > 0) {
+			EnumSet<JSONWriter.Feature> set = EnumSet.noneOf(JSONWriter.Feature.class);
+			Collections.addAll(set, features);
+			Collections.addAll(set, standard);
+			return set.toArray(new JSONWriter.Feature[0]);
+		}
+		return standard;
+	}
+
+	static JSONReader.Feature[] merge(JSONReader.Feature[] standard, final JSONReader.Feature[] features) {
+		if (features.length > 0) {
+			EnumSet<JSONReader.Feature> set = EnumSet.noneOf(JSONReader.Feature.class);
+			Collections.addAll(set, features);
+			Collections.addAll(set, standard);
+			return set.toArray(new JSONReader.Feature[0]);
+		}
+		return standard;
 	}
 
 	public static JSONReader.Feature[] getDefaultReaderFeatures() {
@@ -66,9 +94,16 @@ public class Fastjsons {
 		} catch (Throwable e) {
 			throw new IllegalArgumentException(e);
 		}
-		/*String json = toJsonString(target);
-		return json == null ? new byte[0] : json.getBytes(StandardCharsets.UTF_8);*/
 	}
+
+	public static byte[] toJsonBytes(Object target, JSONWriter.Feature... features) {
+		try {
+			return JSON.toJSONBytes(target, merge(writerFeatures, features));
+		} catch (Throwable e) {
+			throw new IllegalArgumentException(e);
+		}
+	}
+
 
 	public static String toJsonString(Object target) {
 		try {
@@ -78,11 +113,28 @@ public class Fastjsons {
 		}
 	}
 
+	public static String toJsonString(Object target, JSONWriter.Feature... features) {
+		try {
+			return JSON.toJSONString(target, merge(writerFeatures, features));
+		} catch (Throwable e) {
+			throw new IllegalArgumentException(e);
+		}
+	}
+
 	public static String toJsonStringOrNull(Object target) {
 		try {
 			return JSON.toJSONString(target, writerFeatures);
 		} catch (Throwable e) {
-			log.error("json序列化失败" , e);
+			log.error("json序列化失败", e);
+			return null;
+		}
+	}
+
+	public static String toJsonStringOrNull(Object target, JSONWriter.Feature... features) {
+		try {
+			return JSON.toJSONString(target, merge(writerFeatures, features));
+		} catch (Throwable e) {
+			log.error("json序列化失败", e);
 			return null;
 		}
 	}
@@ -95,11 +147,28 @@ public class Fastjsons {
 		}
 	}
 
+	public static String toJsonPrettyString(Object target, JSONWriter.Feature... features) {
+		try {
+			return JSON.toJSONString(target, merge(writerPrettyFeatures, features));
+		} catch (Throwable e) {
+			throw new IllegalArgumentException(e);
+		}
+	}
+
 	public static String toJsonPrettyStringOrNull(Object target) {
 		try {
 			return JSON.toJSONString(target, writerPrettyFeatures);
 		} catch (Throwable e) {
-			log.error("json序列化失败" , e);
+			log.error("json序列化失败", e);
+			return null;
+		}
+	}
+
+	public static String toJsonPrettyStringOrNull(Object target, JSONWriter.Feature... features) {
+		try {
+			return JSON.toJSONString(target, merge(writerPrettyFeatures, features));
+		} catch (Throwable e) {
+			log.error("json序列化失败", e);
 			return null;
 		}
 	}
@@ -112,25 +181,53 @@ public class Fastjsons {
 		}
 	}
 
+	public static String toJsonOrJavaString(Object target, JSONWriter.Feature... features) {
+		try {
+			return JSON.toJSONString(target, merge(writerFeatures, features));
+		} catch (Throwable e) {
+			return target == null ? null : target.toString();
+		}
+	}
+
 	public static Object toJsonObject(Object obj) {
 		return JSON.toJSON(obj, writerFeatures);
+	}
+
+	public static Object toJsonObject(Object obj, JSONWriter.Feature... features) {
+		return JSON.toJSON(obj, merge(writerFeatures, features));
 	}
 
 	public static Object toJson(String text) {
 		return JSON.parse(text, readerFeatures);
 	}
 
+	public static Object toJson(String text, JSONReader.Feature... features) {
+		return JSON.parse(text, merge(readerFeatures, features));
+	}
+
 	public static JSONObject toJsonObject(String text) {
 		return JSON.parseObject(text, readerFeatures);
+	}
+
+	public static JSONObject toJsonObject(String text, JSONReader.Feature... features) {
+		return JSON.parseObject(text, merge(readerFeatures, features));
 	}
 
 	public static JSONArray toJsonList(String text) {
 		return JSON.parseArray(text, readerFeatures);
 	}
 
+	public static JSONArray toJsonList(String text, JSONReader.Feature... features) {
+		return JSON.parseArray(text, merge(readerFeatures, features));
+	}
+
 
 	public static <T> T toJavaObject(String json, TypeRef<T> type) {
 		return toJavaObject(json, type.getType());
+	}
+
+	public static <T> T toJavaObject(String json, TypeRef<T> type, JSONReader.Feature... features) {
+		return toJavaObject(json, type.getType(), features);
 	}
 
 	public static <T> T toJavaObject(String text, Type type) {
@@ -140,12 +237,27 @@ public class Fastjsons {
 		return JSON.parseObject(text, type, readerFeatures);
 	}
 
+	public static <T> T toJavaObject(String text, Type type, JSONReader.Feature... features) {
+		if (type instanceof JavaType) {
+			return toJavaObject(text, ((JavaType<?>) type).getRawType(), features);
+		}
+		return JSON.parseObject(text, type, merge(readerFeatures, features));
+	}
+
 	public static <T> T toJavaObject(String text, Class<T> clazz) {
 		return JSON.parseObject(text, clazz, readerFeatures);
 	}
 
+	public static <T> T toJavaObject(String text, Class<T> clazz, JSONReader.Feature... features) {
+		return JSON.parseObject(text, clazz, merge(readerFeatures, features));
+	}
+
 	public static <T> T toJavaObject(String text, TypeReference<T> clazz) {
 		return JSON.parseObject(text, clazz, readerFeatures);
+	}
+
+	public static <T> T toJavaObject(String text, TypeReference<T> clazz, JSONReader.Feature... features) {
+		return JSON.parseObject(text, clazz, merge(readerFeatures, features));
 	}
 
 	public static <T> List<T> toJavaList(String text, Type type) {
@@ -155,8 +267,19 @@ public class Fastjsons {
 		return JSON.parseArray(text, type, readerFeatures);
 	}
 
+	public static <T> List<T> toJavaList(String text, Type type, JSONReader.Feature... features) {
+		if (type instanceof JavaType) {
+			return toJavaList(text, ((JavaType<?>) type).getRawType(), features);
+		}
+		return JSON.parseArray(text, type, merge(readerFeatures, features));
+	}
+
 	public static <T> List<T> toJavaList(String text, Class<T> clazz) {
 		return JSON.parseArray(text, clazz, readerFeatures);
+	}
+
+	public static <T> List<T> toJavaList(String text, Class<T> clazz, JSONReader.Feature... features) {
+		return JSON.parseArray(text, clazz, merge(readerFeatures, features));
 	}
 
 	public static <T> Type toType(TypeReference<T> type) {
