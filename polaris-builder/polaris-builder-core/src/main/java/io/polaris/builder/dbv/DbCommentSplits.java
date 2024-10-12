88e85ed5dd2611948210557033202a907e9c80be
tianqiang <kylinmania@163.com>
2024-10-12 15:49:15 +0800

    【代码优化】 优化Db列注释标题与内容的切分方法

--- polaris-builder/polaris-builder-core/src/main/java/io/polaris/builder/dbv/DbCommentSplits.java
+++ polaris-builder/polaris-builder-core/src/main/java/io/polaris/builder/dbv/DbCommentSplits.java
@@ -6,7 +6,7 @@ import io.polaris.core.tuple.Tuples;
 
 /**
  * @author Qt
- * @since  Aug 08, 2023
+ * @since Aug 08, 2023
  */
 public class DbCommentSplits {
 
@@ -17,24 +17,28 @@ public class DbCommentSplits {
 	 * @return
 	 */
 	public static Tuple2<String, String> split(String comment) {
-		String columnLabel = Strings.coalesce(comment, "");
+		String columnLabel = Strings.coalesce(comment, "" );
 		String columnRemark = "";
-		int columnLabelSplitIdx = columnLabel.indexOf('\n');
-		if (columnLabelSplitIdx <= 0) {
-			columnLabelSplitIdx = columnLabel.indexOf('(');
-			if (columnLabelSplitIdx <= 0) {
-				columnLabelSplitIdx = columnLabel.indexOf(':');
-			}
-			if (columnLabelSplitIdx <= 0) {
-				columnLabelSplitIdx = columnLabel.indexOf('（');
-			}
-			if (columnLabelSplitIdx <= 0) {
-				columnLabelSplitIdx = columnLabel.indexOf('：');
-			}
-		}
+		int columnLabelSplitIdx = columnLabel.length() - 1;
 		if (columnLabelSplitIdx > 0) {
-			columnRemark = columnLabelSplitIdx < columnLabel.length() - 1 ? columnLabel.substring(columnLabelSplitIdx + 1) : "";
-			columnLabel = columnLabel.substring(0, columnLabelSplitIdx);
+			for (char c : new char[]{'\n', '\r', '（', '(', '：', ':'}) {
+				// 取最小的切分点
+				int i = columnLabel.indexOf(c);
+				if (i >= 0) {
+					columnLabelSplitIdx = Integer.min(i, columnLabelSplitIdx);
+				}
+			}
+			if (columnLabelSplitIdx < columnLabel.length() - 1) {
+				columnRemark = columnLabel.substring(columnLabelSplitIdx + 1).trim();
+				if (!columnRemark.isEmpty()) {
+					char start = columnLabel.charAt(columnLabelSplitIdx);
+					char end = columnRemark.charAt(columnRemark.length() - 1);
+					if ((start == '(' || start == '（') && (end == ')' || end == '）')) {
+						columnRemark = columnRemark.substring(0, columnRemark.length() - 1);
+					}
+				}
+				columnLabel = columnLabel.substring(0, columnLabelSplitIdx).trim();
+			}
 		}
 		return Tuples.of(columnLabel.trim(), columnRemark.trim());
 	}
