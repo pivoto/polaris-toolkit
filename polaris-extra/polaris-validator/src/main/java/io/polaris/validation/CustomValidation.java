a9c8f627023325b132600ca8f68d5b02059483c8
tianqiang <kylinmania@163.com>
2024-10-17 09:15:51 +0800

    【代码优化】 添加代码注释说明

--- polaris-extra/polaris-validator/src/main/java/io/polaris/validation/CustomValidation.java
+++ polaris-extra/polaris-validator/src/main/java/io/polaris/validation/CustomValidation.java
@@ -3,6 +3,25 @@ package io.polaris.validation;
 import javax.validation.ConstraintValidatorContext;
 
 /**
+ * 通过实现此接口，实现{@link CustomValidated}自定义验证。
+ * <pre>
+ * <code>
+ *   class MyCustomValidation implements CustomValidation {
+ *     public boolean isValid(ConstraintValidatorContext context, Object value, String... arguments) {
+ *       // 验证是否通过
+ *       if(isInvalid(value)){
+ *         // 添加自定义错误信息
+ *         context.disableDefaultConstraintViolation();
+ *         context.buildConstraintViolationWithTemplate("自定义错误信息")
+ *                .addPropertyNode("属性名")
+ *                .addConstraintViolation();
+ *       }
+ *       return true;
+ *     }
+ *   }
+ * </code>
+ * </pre>
+ *
  * @author Qt
  * @since 1.8
  */
@@ -12,7 +31,7 @@ public interface CustomValidation {
 	 *
 	 * @param context
 	 * @param value
-	 * @param arguments
+	 * @param arguments {@link CustomValidated#arguments()}
 	 * @return
 	 */
 	boolean isValid(ConstraintValidatorContext context, Object value, String... arguments);
