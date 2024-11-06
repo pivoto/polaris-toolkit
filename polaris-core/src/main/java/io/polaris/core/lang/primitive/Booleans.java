cf6af0f26597d5697e5966f251c8b24a5592f7be
tianqiang <kylinmania@163.com>
2024-11-06 14:07:13 +0800

    【功能扩展】 Booleans

--- polaris-core/src/main/java/io/polaris/core/lang/primitive/Booleans.java
+++ polaris-core/src/main/java/io/polaris/core/lang/primitive/Booleans.java
@@ -13,6 +13,38 @@ import io.polaris.core.random.Randoms;
 public class Booleans {
 
 	/**
+	 * 检查 {@code Boolean} 值是否为 {@code true}
+	 *
+	 * <pre>
+	 *   BooleanUtil.isTrue(Boolean.TRUE)  = true
+	 *   BooleanUtil.isTrue(Boolean.FALSE) = false
+	 *   BooleanUtil.isTrue(null)          = false
+	 * </pre>
+	 *
+	 * @param bool 被检查的Boolean值
+	 * @return 当值为true且非null时返回{@code true}
+	 */
+	public static boolean isTrue(Boolean bool) {
+		return Boolean.TRUE.equals(bool);
+	}
+
+	/**
+	 * 检查 {@code Boolean} 值是否为 {@code false}
+	 *
+	 * <pre>
+	 *   BooleanUtil.isFalse(Boolean.TRUE)  = false
+	 *   BooleanUtil.isFalse(Boolean.FALSE) = true
+	 *   BooleanUtil.isFalse(null)          = false
+	 * </pre>
+	 *
+	 * @param bool 被检查的Boolean值
+	 * @return 当值为false且非null时返回{@code true}
+	 */
+	public static boolean isFalse(Boolean bool) {
+		return Boolean.FALSE.equals(bool);
+	}
+
+	/**
 	 * 数组是否为空
 	 *
 	 * @param array 数组
