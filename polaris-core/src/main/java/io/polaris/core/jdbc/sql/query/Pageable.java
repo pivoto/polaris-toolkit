83f5a6bffee9f76617cf268e8d0543fc7652e8e4
tianqiang <kylinmania@163.com>
2024-11-06 10:04:37 +0800

    【功能扩展】 Pageable

--- polaris-core/src/main/java/io/polaris/core/jdbc/sql/query/Pageable.java
+++ polaris-core/src/main/java/io/polaris/core/jdbc/sql/query/Pageable.java
@@ -1,18 +1,12 @@
 package io.polaris.core.jdbc.sql.query;
 
 /**
+ * 页号从1开始
+ *
  * @author Qt
- * @since  Aug 29, 2023
+ * @since Aug 29, 2023
  */
 public interface Pageable {
-
-	static Pageable newInstance() {
-		return new Pagination();
-	}
-
-	static Pageable newInstance(int pageNum, int pageSize) {
-		return new Pagination(pageNum, pageSize);
-	}
 
 	int getPageNum();
 
@@ -29,5 +23,101 @@ public interface Pageable {
 	OrderBy getOrderBy();
 
 	void setOrderBy(OrderBy orderBy);
+
+	/**
+	 * 获取起始数据下标，从0开始
+	 *
+	 * @return
+	 */
+	default int startResult() {
+		return (getPageNum() - 1) * getPageSize();
+	}
+
+	/**
+	 * 获取结束数据下标，从0开始
+	 *
+	 * @return
+	 */
+	default int endResult() {
+		return getPageNum() * getPageSize() - 1;
+	}
+
+	/**
+	 * 获取起始数据行号，从1开始
+	 *
+	 * @return
+	 */
+	default int startRow() {
+		return (getPageNum() - 1) * getPageSize() + 1;
+	}
+
+	/**
+	 * 获取结束数据行号，从1开始
+	 *
+	 * @return
+	 */
+	default int endRow() {
+		return getPageNum() * getPageSize();
+	}
+
+	/**
+	 * 创建分页对象
+	 *
+	 * @return
+	 */
+	static Pageable newInstance() {
+		return new Pagination();
+	}
+
+	/**
+	 * 创建分页对象
+	 *
+	 * @param pageNum  页号
+	 * @param pageSize 页大小
+	 * @return
+	 */
+	static Pageable newInstance(int pageNum, int pageSize) {
+		return new Pagination(pageNum, pageSize);
+	}
+
+	/**
+	 * 获取起始数据下标，从0开始
+	 *
+	 * @param page 分页对象
+	 * @return
+	 */
+	static int getStartResult(Pageable page) {
+		return (page.getPageNum() - 1) * page.getPageSize();
+	}
+
+	/**
+	 * 获取结束数据下标，从0开始
+	 *
+	 * @param page 分页对象
+	 * @return
+	 */
+	static int getEndResult(Pageable page) {
+		return page.getPageNum() * page.getPageSize() - 1;
+	}
+
+	/**
+	 * 获取起始数据行号，从1开始
+	 *
+	 * @param page 分页对象
+	 * @return
+	 */
+	static int getStartRow(Pageable page) {
+		return (page.getPageNum() - 1) * page.getPageSize() + 1;
+	}
+
+	/**
+	 * 获取结束数据行号，从1开始
+	 *
+	 * @param page 分页对象
+	 * @return
+	 */
+	static int getEndRow(Pageable page) {
+		return page.getPageNum() * page.getPageSize();
+	}
 
 }
