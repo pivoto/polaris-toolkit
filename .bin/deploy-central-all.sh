fec320aa92ab196b60bc1663d60735dfd72a079d
tianqiang <kylinmania@163.com>
2025-08-07 19:54:25 +0800

    【项目构建】 调整脚本

--- .bin/deploy-central-all.sh
+++ .bin/deploy-central-all.sh
@@ -3,7 +3,6 @@
 set -Ee
 set -uo pipefail
 
-grep -E '<version>' $(cd $(dirname $0) && pwd -P)/pom.xml  --max-count=1 >> $(cd $(dirname $0) && pwd -P)/version.log
 sh $(cd $(dirname $0) && pwd -P)/main.sh -central -g deploy
 sh $(cd $(dirname $0) && pwd -P)/change-central-pivoto.sh
 sh $(cd $(dirname $0) && pwd -P)/main.sh -central -g deploy
--- .bin/deploy-central-pivoto.sh
+++ .bin/deploy-central-pivoto.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-grep -E '<version>' $(cd $(dirname $0) && pwd -P)/pom.xml  --max-count=1 >> $(cd $(dirname $0) && pwd -P)/version.log
 sh $(cd $(dirname $0) && pwd -P)/change-central-pivoto.sh
 sh $(cd $(dirname $0) && pwd -P)/main.sh -central -g deploy
 sh $(cd $(dirname $0) && pwd -P)/change-central.sh
