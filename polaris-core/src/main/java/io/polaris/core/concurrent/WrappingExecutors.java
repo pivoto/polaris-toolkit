91a9ed843184a083947cd2d5dbc8fd90168c608a
tianqiang <kylinmania@163.com>
2025-10-22 10:06:18 +0800

    【功能扩展】 WrappingExecutors添加默认WrappingTaskFactory构建方法并优化线程池创建
    - 新增buildDefaultWrappingTaskFactory方法，支持通过ServiceLoader加载拦截器和监听器
    - 实现getDefaultWrappingTaskFactory方法的懒加载逻辑
    - 所有create方法改为使用getDefaultWrappingTaskFactory替代直接使用defaultWrappingTaskFactory变量
    - 添加Nonnull注解和相关依赖导入

--- polaris-core/src/main/java/io/polaris/core/concurrent/WrappingExecutors.java
+++ polaris-core/src/main/java/io/polaris/core/concurrent/WrappingExecutors.java
@@ -5,6 +5,10 @@ import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
+import javax.annotation.Nonnull;
+
+import io.polaris.core.service.ServiceLoader;
+
 /**
  * @author Qt
  * @since Jan 24, 2025
@@ -12,7 +16,24 @@ import java.util.concurrent.TimeUnit;
 public class WrappingExecutors extends Executors {
 	private static WrappingTaskFactory defaultWrappingTaskFactory;
 
+	@Nonnull
+	public static DefaultWrappingTaskFactory buildDefaultWrappingTaskFactory() {
+		DefaultWrappingTaskFactory factory = new DefaultWrappingTaskFactory();
+		WrappingInterceptor interceptor = ServiceLoader.of(WrappingInterceptor.class).getPureSingleton();
+		if (interceptor != null) {
+			factory.setInterceptor(interceptor);
+		}
+		WrappingExecutionListener listener = ServiceLoader.of(WrappingExecutionListener.class).getPureSingleton();
+		if (listener != null) {
+			factory.setListener(listener);
+		}
+		return factory;
+	}
+
 	public static WrappingTaskFactory getDefaultWrappingTaskFactory() {
+		if (defaultWrappingTaskFactory == null) {
+			defaultWrappingTaskFactory = buildDefaultWrappingTaskFactory();
+		}
 		return defaultWrappingTaskFactory;
 	}
 
@@ -22,50 +43,50 @@ public class WrappingExecutors extends Executors {
 
 
 	public static ThreadPoolExecutor create(int core, final String threadNamePrefix) {
-		return create(defaultWrappingTaskFactory, core, core, threadNamePrefix, true);
+		return create(getDefaultWrappingTaskFactory() , core, core, threadNamePrefix, true);
 	}
 
 	public static ThreadPoolExecutor create(int core, int max, final String threadNamePrefix) {
-		return create(defaultWrappingTaskFactory, core, max, threadNamePrefix, true);
+		return create(getDefaultWrappingTaskFactory() , core, max, threadNamePrefix, true);
 	}
 
 	public static ThreadPoolExecutor create(int core, final String threadNamePrefix, final boolean isDaemon) {
-		return create(defaultWrappingTaskFactory, core, core, createDefaultBlockingQueue(), threadNamePrefix, isDaemon);
+		return create(getDefaultWrappingTaskFactory() , core, core, createDefaultBlockingQueue(), threadNamePrefix, isDaemon);
 	}
 
 	public static ThreadPoolExecutor create(int core, int max, final String threadNamePrefix, final boolean isDaemon) {
-		return create(defaultWrappingTaskFactory, core, max, createDefaultBlockingQueue(), threadNamePrefix, isDaemon);
+		return create(getDefaultWrappingTaskFactory() , core, max, createDefaultBlockingQueue(), threadNamePrefix, isDaemon);
 	}
 
 	public static ThreadPoolExecutor create(int core, BlockingQueue<Runnable> blockingQueue, final String threadNamePrefix, final boolean isDaemon) {
-		return create(defaultWrappingTaskFactory, core, core, blockingQueue, new PooledThreadFactory().withPrefix(threadNamePrefix).withDaemon(isDaemon)
+		return create(getDefaultWrappingTaskFactory() , core, core, blockingQueue, new PooledThreadFactory().withPrefix(threadNamePrefix).withDaemon(isDaemon)
 		);
 	}
 
 	public static ThreadPoolExecutor create(int core, int max, BlockingQueue<Runnable> blockingQueue, final String threadNamePrefix, final boolean isDaemon) {
-		return create(defaultWrappingTaskFactory, core, max, blockingQueue, new PooledThreadFactory().withPrefix(threadNamePrefix).withDaemon(isDaemon)
+		return create(getDefaultWrappingTaskFactory() , core, max, blockingQueue, new PooledThreadFactory().withPrefix(threadNamePrefix).withDaemon(isDaemon)
 		);
 	}
 
 
 	public static ThreadPoolExecutor create(int core, ThreadFactory threadFactory) {
-		return create(defaultWrappingTaskFactory, core, core, createDefaultBlockingQueue(), threadFactory);
+		return create(getDefaultWrappingTaskFactory() , core, core, createDefaultBlockingQueue(), threadFactory);
 	}
 
 	public static ThreadPoolExecutor create(int core, int max, ThreadFactory threadFactory) {
-		return create(defaultWrappingTaskFactory, core, max, createDefaultBlockingQueue(), threadFactory);
+		return create(getDefaultWrappingTaskFactory() , core, max, createDefaultBlockingQueue(), threadFactory);
 	}
 
 	public static ThreadPoolExecutor create(int core, BlockingQueue<Runnable> blockingQueue, ThreadFactory threadFactory) {
-		return create(defaultWrappingTaskFactory, core, core, blockingQueue, threadFactory);
+		return create(getDefaultWrappingTaskFactory() , core, core, blockingQueue, threadFactory);
 	}
 
 	public static ThreadPoolExecutor create(int core, int max, BlockingQueue<Runnable> blockingQueue, ThreadFactory threadFactory) {
-		return create(defaultWrappingTaskFactory, core, max, blockingQueue, threadFactory);
+		return create(getDefaultWrappingTaskFactory() , core, max, blockingQueue, threadFactory);
 	}
 
 	public static ThreadPoolExecutor create(int core, int max, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> blockingQueue, ThreadFactory threadFactory) {
-		return create(defaultWrappingTaskFactory, core, max, keepAliveTime, unit, blockingQueue, threadFactory);
+		return create(getDefaultWrappingTaskFactory() , core, max, keepAliveTime, unit, blockingQueue, threadFactory);
 	}
 
 }
