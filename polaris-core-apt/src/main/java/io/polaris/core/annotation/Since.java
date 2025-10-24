7d77468bc212fdb0ffd5073fbccabe4683bc3077
tianqiang <kylinmania@163.com>
2025-10-24 16:27:36 +0800

    【功能新增】 添加 @Since 注解用于标识版本

---
+++ polaris-core-apt/src/main/java/io/polaris/core/annotation/Since.java
@@ -0,0 +1,22 @@
+package io.polaris.core.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 标识该类或方法从哪个版本开始可用
+ *
+ * @author Qt
+ * @since 1.8
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.METHOD, ElementType.CONSTRUCTOR, ElementType.ANNOTATION_TYPE, ElementType.FIELD})
+public @interface Since {
+
+	String[] value() default "";
+
+}
